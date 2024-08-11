83b7e5309751d3ce2a72f13f6ef5c794bf32e8c9
Jeffrey Lluberes <141197852+JeffLluberes@users.noreply.github.com>
2024-08-11 12:16:15 -0400

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,82 @@
+package TestCases;
+
+import Pages.Homepage;
+import Pages.LoginPage;
+import Pages.MyAccountPage;
+import TestBase.TestBase;
+import net.bytebuddy.build.Plugin;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Login extends TestBase {
+    public Login() throws Exception {
+        super();
+    }
+    public WebDriver driver;
+    public Homepage homepage;
+    public LoginPage loginPage;
+    public MyAccountPage myAccountPage;
+
+
+
+    @BeforeMethod
+    public void loginSetup(){
+
+        driver = initializeBrowserAndOpenApplication(prop.getProperty("browser"));
+        homepage = new Homepage(driver);
+        homepage.clickMyAccountDropDown();
+        homepage.clickloginLink();
+    }
+    @Test(priority = 1)
+    public void verifyLoginWithValidCredentials(){
+        loginPage = new LoginPage(driver);
+        loginPage.enterEmail(prop.getProperty("validEmail"));
+        loginPage.enterPassword(prop.getProperty("validPassword"));
+        loginPage.clickOnLoginButton();
+        myAccountPage = new MyAccountPage(driver);
+        myAccountPage.editAccountInfoLinkIsDisplayed();
+
+    }
+
+    @Test(priority = 2)
+    public void verifyForgotPasswordLink(){
+    loginPage = new LoginPage(driver);
+    loginPage.forgotPassword();
+    loginPage.enterEmailToForgotPasswordFieldBox(prop.getProperty("validEmail"));
+    loginPage.continueButtonForgotEmailPage();
+        Assert.assertTrue(loginPage.confirmationLinkSentToEmailIsDisplayed());
+
+    }
+    @Test(priority = 3)
+    public void loginWithInvalidEmailValidPassword(){
+        loginPage = new LoginPage(driver);
+        loginPage.enterEmail(dataProp.getProperty("invalidEmail"));
+        loginPage.enterPassword(prop.getProperty("validPassword"));
+        loginPage.clickOnLoginButton();
+        Assert.assertTrue(loginPage.retrieveWarningEmailPasswordMismatchText());
+
+    }
+    @Test(priority = 4)
+    public void loginWithValidEmailInvalidPassword() {
+        loginPage = new LoginPage(driver);
+        loginPage.enterEmail(prop.getProperty("validEmail"));
+        loginPage.enterPassword(dataProp.getProperty("invalidPassword"));
+        loginPage.clickOnLoginButton();
+        Assert.assertTrue(loginPage.retrieveWarningEmailPasswordMismatchText());
+    }
+    @Test(priority = 5)
+    public void loginWithInvalidEmailInvalidPassword(){
+        loginPage = new LoginPage(driver);
+        loginPage.enterEmail(dataProp.getProperty("invalidEmail"));
+        loginPage.enterPassword(dataProp.getProperty("invalidPassword"));
+        loginPage.clickOnLoginButton();
+        Assert.assertTrue(loginPage.retrieveWarningEmailPasswordMismatchText());
+    }
+    @AfterMethod
+    public void tearDOwn(){
+        driver.quit();
+    }
+}
